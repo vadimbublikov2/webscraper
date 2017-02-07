36dc6efacb2bf60517b95a19497a944a0469df4e
Vadim Bublikov <vadimbublikov@narod.ru>
2017-02-07 12:12:49 +0300

    +Scraper

---
+++ ru/bvd/ws/Scraper.java
@@ -0,0 +1,31 @@
+package ru.bvd.ws;
+
+import java.util.ArrayList;
+
+class Scraper {
+    //---------static
+    static final Scraper INSTANCE = new Scraper();
+    static final String man = " webscraper - console web scraper (http://en.wikipedia.org/wiki/Web_scraping)\n"+
+            "\n"+
+            " SYNOPSIS\n"+
+            "   webscraper [URL] [-f URLs file] WORDS [OPTIONS]\n"+
+            " OPTIONS\n"+
+            "    -v output verbosity flag,  if on then the output should contains information about time spend on data scraping and data processing\n"+
+            "    -w count number of provided word(s) occurrence on webpage(s)\n"+
+            "    -c count number of characters of each web page\n"+
+            "    -e extract sentences’ which contain given words\n"+
+            " EXAMPLE\n"+
+            "    java –jar scraper.jar http://www.cnn.com Trump,Trump's –v –w –c –e\n"+
+            "    java –jar scraper.jar -f urls.txt Greece,default –v –w –c –e\n" +
+            " OUTPUT FORMAT\n" +
+            "    URL | word | tag | sentence";
+
+    static class Documents extends ArrayList<Document> {
+        @Override
+        public synchronized boolean add(Document document) {
+            return super.add(document);
+        }
+    }
+    static class Words extends ArrayList<String>{}
+    static class Includes extends ArrayList<Include>{}
+}
