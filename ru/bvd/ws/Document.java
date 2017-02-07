3847651ac0eb36856688b7dabfaf11740d5d0430
Vadim Bublikov <vadimbublikov@narod.ru>
2017-02-07 11:59:37 +0300

    +Document

--- ru/bvd/ws/Document.java
+++ ru/bvd/ws/Document.java
@@ -3,16 +3,73 @@ package ru.bvd.ws;
 import java.time.Duration;
 
 public class Document {
-    String url;
-    int httpStatus = 0;
-    String content;
-    String statusError;
-    Duration getDuration;
-    Duration processDuration;
-    int wordsCount = 0;
+    private String url;
+    private int httpStatus = 0;
+    private String content;
+    private String statusError;
+    private Duration getDuration;
+    private Duration processDuration;
+    private int wordsCount = 0;
 
     public Document(String url) {
         this.url = url;
+    }
+
+    //get/set
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setHttpStatus(int httpStatus) {
+        this.httpStatus = httpStatus;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setStatusError(String statusError) {
+        this.statusError = statusError;
+    }
+
+    public void setGetDuration(Duration getDuration) {
+        this.getDuration = getDuration;
+    }
+
+    public void setProcessDuration(Duration processDuration) {
+        this.processDuration = processDuration;
+    }
+
+    public void setWordsCount(int wordsCount) {
+        this.wordsCount = wordsCount;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public int getHttpStatus() {
+        return httpStatus;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getStatusError() {
+        return statusError;
+    }
+
+    public Duration getGetDuration() {
+        return getDuration;
+    }
+
+    public Duration getProcessDuration() {
+        return processDuration;
+    }
+
+    public int getWordsCount() {
+        return wordsCount;
     }
 
     @Override
